8327b1a4209bf5757ca08593c7b83ca42875d55a
parth Devani <devaniparthg@gmail.com>
2021-07-16 14:20:10 +0530

    body-parser, cors and morgan added

--- app.js
+++ app.js
@@ -2,8 +2,36 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = (process.env.PORT || '8000')
+const bodyParser = require("body-parser")
+const cors = require('cors')
+var logger = require('morgan')
 
-app.get('/', (req, res) => {
+//enable CORS with various options.
+app.use(cors())
+// app.options('*', cors())
+//app.use(cors({ origin: true }));
+
+//json allow
+app.use(bodyParser.json({limit: '50mb'}))
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}))
+
+//display time and service name in terminal
+//:method :url :status :response-time ms - :res[content-length]
+app.use(logger('dev'))
+
+app.use(function (req, res, next) {
+  //Enabling CORS
+  process.env.Version = req.url.split('/')[2]
+  // res.header("Access-Control-Allow-Origin", "*")
+  // res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization")
+  next()
+});
+
+
+app.get('/api', (req, res) => {
   res.send('Hello World!...')
 })
 
